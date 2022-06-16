89cc4b27a8be7da15ce460d8b940b762b92676cb
Youssef Hourri <youssefhourri69@gmail.com>
2022-06-16 15:55:22 +0200

    modif Controller Avis

--- src/Controller/AvisController.php
+++ src/Controller/AvisController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+
+use App\Repository\OwnerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AvisController extends AbstractController
 {
-    #[Route('/avis', name: 'app_avis')]
-    public function index(): Response
+    #[Route('/avis/{id}', name: 'app_avis', requirements: ['id' => '\d+'])]
+    public function index(int $id,OwnerRepository $ownerRepository): Response
     {
         return $this->render('avis/index.html.twig', [
-            'controller_name' => 'AvisController',
+            'avis' => $ownerRepository->find($id),
         ]);
     }
 }
