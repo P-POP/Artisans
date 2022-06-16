ecbddcbc20d2f8dc5fbb2344e4a10772cf357e7e
Megane SMAILI <megane.smaili.dev@gmail.com>
2022-06-16 15:28:07 +0200

    reparation visions

--- src/Controller/ArtisanController.php
+++ src/Controller/ArtisanController.php
@@ -14,13 +14,14 @@ class ArtisanController extends AbstractController
     #[Route('/artisan', name: 'app_artisan')]
     public function index(ArtisanRepository $artisanRepository, PaginatorInterface $paginatorInterface, Request $request ): Response
     {
-        $artisans = $paginatoreInterface->paginate(
+        $artisans = $paginatorInterface->paginate(
             $artisanRepository->findAll(), //Requête SQL/DQL
             $request->query->getInt('page', 1), //Numéritation des pages 
             $request->query->getInt('numbers', 5) //Nombre d'enregistrement par page
         );
         return $this->render('artisan/index.html.twig', [
-            'controller_name' => 'ArtisanController',
+            'artisans' => $artisans,
+            'details' => $artisanRepository->find(5)
         ]);
     }
 }
