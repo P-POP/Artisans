822a3b6b9daf766813f4709734d33d201ef23b7f
Fontaine Gaetan <fontaine_gaetan@orange.fr>
2022-06-16 13:23:43 +0200

    fixtures

--- src/DataFixtures/OwnerFixtures.php
+++ src/DataFixtures/OwnerFixtures.php
@@ -7,7 +7,7 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class OwnerFixtures extends Fixture implements DependentFixtureInterface;
+class OwnerFixtures extends Fixture implements DependentFixtureInterface
 {
 
     public function load(ObjectManager $manager): void
