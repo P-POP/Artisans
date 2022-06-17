e2a5ebc9de9d28291b5e93dde7bd80f1b4ecbbe2
Youssef Hourri <youssefhourri69@gmail.com>
2022-06-17 11:55:15 +0200

    npm build

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
