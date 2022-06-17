68d37ba8728e8e4c866be45f39650e3e1dfaf8b4
Fontaine Gaetan <fontaine_gaetan@orange.fr>
2022-06-17 14:19:55 +0200

    Mise a jour avis

--- src/Controller/AvisController.php
+++ src/Controller/AvisController.php
@@ -2,15 +2,9 @@
 
 namespace App\Controller;
 
-<<<<<<< HEAD
-use App\Entity\Artisan;
-use App\Entity\Owner;
-use App\Entity\Type;
-use App\Form\OwnerFormType;
-use App\Repository\ArtisanRepository;
-=======
 
->>>>>>> a8ed7e2f60d800a5ccd7afe95514ee62cf8aed27
+use App\Entity\Owner;
+use App\Form\OwnerFormType;
 use App\Repository\OwnerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,7 +14,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AvisController extends AbstractController
 {
-<<<<<<< HEAD
     #[Route('/avis', name: 'app_avis', requirements:['id' => '\d+'])]
     public function index(Request $request, OwnerRepository $ownerRepository): Response
     {
@@ -40,13 +33,6 @@ class AvisController extends AbstractController
             return $this->render('avis/index.html.twig', [
                 'form'=> $form->createView()
 
-=======
-    #[Route('/avis/{id}', name: 'app_avis', requirements: ['id' => '\d+'])]
-    public function index(int $id,OwnerRepository $ownerRepository): Response
-    {
-        return $this->render('avis/index.html.twig', [
-            'owner' => $ownerRepository->find($id)
->>>>>>> a8ed7e2f60d800a5ccd7afe95514ee62cf8aed27
         ]);
     }
 }
