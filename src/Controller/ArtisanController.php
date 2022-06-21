18e35362d2e3b136a60e55d30f8218ad1c7c5021
Megane SMAILI <megane.smaili.dev@gmail.com>
2022-06-21 16:23:54 +0200

    artisan repared

--- src/Controller/ArtisanController.php
+++ src/Controller/ArtisanController.php
@@ -112,14 +112,6 @@ class ArtisanController extends AbstractController
         return $this->render('artisan/detailsArtisan.html.twig', [
            'artisanAdress'=> $mapOneAddress,
            "artisan" => $artisan
-    //Création de la route pour le detail des avis concernant un artisan
-    #[Route('/artisan/{id}', name: 'details_artisan', requirements:['id' => '\d+'])]
-    public function details(Artisan $artisan): Response
-    {
-		$i=1;
-        
-        return $this->render('artisan/details.html.twig', [
-            'artisans' => $artisan
         ]);
     }
 
