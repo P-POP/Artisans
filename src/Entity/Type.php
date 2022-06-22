55bf9acc919c201d04e2efb61f6732d9a9a79e81
Fontaine Gaetan <fontaine_gaetan@orange.fr>
2022-06-22 10:13:16 +0200

    maj entity type

--- src/Entity/Type.php
+++ src/Entity/Type.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\TypeRepository;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TypeRepository::class)]
@@ -15,6 +16,9 @@ class Type
 
     #[ORM\Column(type: 'string', length: 50)]
     private $name;
+    
+    #[ORM\OneToMany(mappedBy: 'artisan', targetEntity: Artisan::class, orphanRemoval: true)]
+    private $artisan;
 
     public function getId(): ?int
     {
@@ -29,6 +33,36 @@ class Type
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+     /**
+     * @return Collection<int, Artisan>
+     */
+    public function getArtisan(): Collection
+    {
+        return $this->artisan;
+    }
+
+    public function addArtisan(Artisan $artisan): self
+    {
+        if (!$this->artisan->contains($artisan)) {
+            $this->artisan[] = $artisan;
+            $artisan->setType($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMagazine(Artisan $artisan): self
+    {
+        if ($this->artisan->removeElement($artisan)) {
+            // set the owning side to null (unless already changed)
+            if ($artisan->getType() === $this) {
+                $artisan->setType(null);
+            }
+        }
 
         return $this;
     }
